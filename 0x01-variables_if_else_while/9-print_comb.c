ab554739c0e506693d542332e4a99d72304e94f4
azmy1999 <mo@example.com>
2023-07-17 16:37:07 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,42 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
-int main() {
-// use putchar to print numbers from 0 to 9 separated by ", "
-putchar('0');
-putchar(',');
-putchar(' ');
-putchar('1');
-putchar(',');
-putchar(' ');
 
-putchar('2');
-putchar(',');
-putchar(' ');
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
 
-putchar('3');
-putchar(',');
-putchar(' ');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-putchar('4');
-putchar(',');
-putchar(' ');
+	putchar('\n');
 
-putchar('5');
-putchar(',');
-putchar(' ');
-
-putchar('6');
-putchar(',');
-putchar(' ');
-
-putchar('7');
-putchar(',');
-putchar(' ');
-
-putchar('8');
-putchar(',');
-putchar(' ');
-
-putchar('9');
-
-return 0;
+	return (0);
 }
